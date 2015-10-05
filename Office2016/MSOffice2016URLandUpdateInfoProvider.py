23c6388d6b8e1375132b408c4877ab34824e77ba
Ubertec <service@ubertec.co.nz>
2015-10-06 09:03:24 +1300

    Update MSOffice2016URLandUpdateInfoProvider.py

--- Office2016/MSOffice2016URLandUpdateInfoProvider.py
+++ Office2016/MSOffice2016URLandUpdateInfoProvider.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 #
-# Copyright 2015 Allister Banks, wholesale lifted from code by Greg Neagle
+# Copyright 2015 Allister Banks and Tim Sutton,
+# based on MSOffice2011UpdateInfoProvider by Greg Neagle
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -13,13 +14,14 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-
+# Disabling 'no-env-member' for recipe processors
+#pylint:disable=e1101
+"""See docstring for MSOffice2016URLandUpdateInfoProvider class"""
 
 import plistlib
 import urllib2
 import re
 
-# from distutils.version import LooseVersion
 from operator import itemgetter
 
 from autopkglib import Processor, ProcessorError
@@ -27,101 +29,115 @@ from autopkglib import Processor, ProcessorError
 
 __all__ = ["MSOffice2016URLandUpdateInfoProvider"]
 
+# CULTURE_CODE defaulting to 'en-US' as the installers and updates seem to be
+# multilingual.
 CULTURE_CODE = "0409"
 BASE_URL = "http://www.microsoft.com/mac/autoupdate/%s15.xml"
 PROD_DICT = {
     'Excel':'XCEL',
+    'OneNote':'ONMC',
+    'Outlook':'OPIM',
     'PowerPoint':'PPT3',
     'Word':'MSWD',
-    'OneNote':'ONMC'
 }
+LOCALE_ID_INFO_URL = "https://msdn.microsoft.com/en-us/goglobal/bb964664.aspx"
 
 class MSOffice2016URLandUpdateInfoProvider(Processor):
-    """Provides a download URL for the most recent version of MS Outlook."""
+    """Provides a download URL for the most recent version of MS Office 2016."""
     input_variables = {
+        "locale_id": {
+            "required": False,
+            "default": "1033",
+            "description": (
+                "Locale ID that determines the language "
+                "that is retrieved from the metadata, currently only "
+                "used by the update description. See %s "
+                "for a list of locale codes. The default is en-US."
+                % LOCALE_ID_INFO_URL)
+        },
         "product": {
             "required": True,
             "description": "Name of product to fetch, e.g. Excel.",
         },
-        "culture_code": {
-            "required": False,
-            "description": ("See "
-                "http://msdn.microsoft.com/en-us/library/ee825488(v=cs.20).aspx"
-                " for a table of CultureCodes. Defaults to 0409, which "
-                "corresponds to en-US (English - United States)"),
-        },
         "version": {
             "required": False,
-            "description": "Update version number. Defaults to latest.",
-        },
-        "base_url": {
-            "required": False,
-            "description": ("Default is %s. If this is given, culture_code "
-                "is ignored." % (BASE_URL % CULTURE_CODE)),
-        },
-        "munki_update_name": {
-            "required": False,
-            "description": (
-                "Name for the update in Munki repo. Defaults to product name + '2016 Installer'"),
+            "default": "latest",
+            "description": ("Update version to fetch. Currently the only "
+                            "supported value is 'latest', which is the "
+                            "default."),
         },
     }
     output_variables = {
-        "url": {
-            "description": "URL to the latest installer.",
-        },
-        "pkg_name": {
-            "description": "Name of the package.",
-        },
         "additional_pkginfo": {
             "description":
                 "Some pkginfo fields extracted from the Microsoft metadata.",
         },
+        "description": {
+            "description":
+                "Description of the update from the manifest, in the language "
+                "given by the locale_id input variable.",
+        },
+        "version": {
+            "description":
+                ("The version of the update as extracted from the Microsoft "
+                 "metadata.")
+        },
+        "minimum_os_version": {
+            "description":
+                ("The minimum os version required by the update as extracted "
+                 "from the Microsoft metadata.")
+        },
+        "url": {
+            "description": "URL to the latest installer.",
+        },
     }
     description = __doc__
 
-    def sanityCheckExpectedTriggers(self, item):
+    def sanity_check_expected_triggers(self, item):
         """Raises an exeception if the Trigger Condition or
         Triggers for an update don't match what we expect.
         Protects us if these change in the future."""
-        # Hopefully this breaks soon, if they stop using "Registered File" placeholders,
-        # but it's not buying us much nor utilized at present
+        # MS currently uses "Registered File" placeholders, which get replaced
+        # with the bundle of a given application ID. In other words, this is
+        # the bundle version of the app itself.
         if not item.get("Trigger Condition") == ["and", "Registered File"]:
             raise ProcessorError(
                 "Unexpected Trigger Condition in item %s: %s"
                 % (item["Title"], item["Trigger Condition"]))
         if not "Registered File" in item.get("Triggers", {}):
             raise ProcessorError(
-                "Missing expected MCP Trigger in item %s" % item["Title"])
+                "Missing expected 'and Registered File' Trigger in item "
+                "%s" % item["Title"])
 
-    def getInstallsItems(self, item):
-        """Attempts to parse the Triggers to create an installs item"""
-        # currently unused, and unnecessary as receipts are sufficient... for now...
-        self.sanityCheckExpectedTriggers(item)
-        triggers = item.get("Triggers", {})
-        paths = [triggers[key].get("File") for key in triggers.keys()]
-        # if "Contents/Info.plist" in paths:
-            # use the apps info.plist as installs item
+    def get_installs_items(self, item):
+        """Attempts to parse the Triggers to create an installs item using
+        only manifest data, making the assumption that CFBundleVersion and
+        CFBundleShortVersionString are equal."""
+        self.sanity_check_expected_triggers(item)
+        version = self.get_version(item)
         installs_item = {
-            "CFBundleShortVersionString": self.getVersion(item),
-            "CFBundleVersion": self.getVersion(item),
+            "CFBundleShortVersionString": version,
+            "CFBundleVersion": version,
             "path": ("/Applications/Microsoft %s.app" % self.env["product"]),
             "type": "application",
-            "version": self.getVersion(item),
-            "version_comparison_key": "CFBundleShortVersionString"
         }
         return [installs_item]
-        # return None
 
-    def getVersion(self, item):
+    def get_version(self, item):
         """Extracts the version of the update item."""
-        # currentlyrelies on the item having the version at  the end of the title,
+        # We currently expect the version at the end of the Title key,
         # e.g.: "Microsoft Excel Update 15.10.0"
-        value_to_parse = item["Title"]
-        just_minor = re.search("( Update )(\d+\.\d+\.\d+)", value_to_parse)
-        version_str = just_minor.group(2)
-        return version_str
+        # item["Title"] = "Microsoft Excel Update 15.10"
+        match = re.search(
+            r"( Update )(?P<version>\d+\.\d+(\.\d)*)", item["Title"])
+        if not match:
+            raise ProcessorError(
+                "Error validating Office 2016 version extracted "
+                "from Title manifest value: '%s'" % item["Title"])
+        version = match.group('version')
+        return version
 
-    def valueToOSVersionString(self, value):
+    def value_to_os_version_string(self, value):
         """Converts a value to an OS X version number"""
         if isinstance(value, int):
             version_str = hex(value)[2:]
@@ -146,87 +162,75 @@ class MSOffice2016URLandUpdateInfoProvider(Processor):
             raise ProcessorError("Unexpected value in version: %s" % value)
         return "%s.%s.%s" % (major, minor, patch)
 
-    def getInstallerinfo(self):
+    def get_installer_info(self):
         """Gets info about an installer from MS metadata."""
-        culture_code = self.env.get("culture_code", CULTURE_CODE)
-        produit = self.env.get("product")
-        prod_code = PROD_DICT.get(produit)
-        base_url = BASE_URL % (culture_code + prod_code)
-        version_str = self.env.get("version")
-        if not version_str:
-            version_str = "latest"
+        base_url = BASE_URL % (CULTURE_CODE + PROD_DICT[self.env["product"]])
+        version_str = self.env["version"]
         # Get metadata URL
         req = urllib2.Request(base_url)
-        # Add the MAU User-Agent, since MAU feed server seems to explicitly block
-        # a User-Agent of 'Python-urllib/2.7' - even a blank User-Agent string
-        # passes.
+        # Add the MAU User-Agent, since MAU feed server seems to explicitly
+        # block a User-Agent of 'Python-urllib/2.7' - even a blank User-Agent
+        # string passes.
         req.add_header("User-Agent",
-            "Microsoft%20AutoUpdate/3.0.6 CFNetwork/720.2.4 Darwin/14.4.0 (x86_64)")
+                       "Microsoft%20AutoUpdate/3.0.6 CFNetwork/720.2.4 Darwin/14.4.0 (x86_64)")
         try:
-            f = urllib2.urlopen(req)
-            data = f.read()
-            f.close()
+            fdesc = urllib2.urlopen(req)
+            data = fdesc.read()
+            fdesc.close()
         except BaseException as err:
             raise ProcessorError("Can't download %s: %s" % (base_url, err))
 
         metadata = plistlib.readPlistFromString(data)
         if version_str == "latest":
-            # Outlook 'update' metadata is a list of dicts.
-            # we need to sort by date.
+            # Still sort by date, in case we should ever need to support
+            # fetching versions other than 'latest'.
             sorted_metadata = sorted(metadata, key=itemgetter('Date'))
             # choose the last item, which should be most recent.
             item = sorted_metadata[-1]
-        else:
-            # we've been told to find a specific version. Unfortunately, the
-            # Outlook updates metadata items don't have a version attibute.
-            # The version is only in text in the update's Title. So we look for
-            # that...although as of January 2015 it's not included...
-            # Titles would normally be in the format "Outlook x.y.z Update"
-            padded_version_str = " " + version_str + " "
-            matched_items = [item for item in metadata
-                            if padded_version_str in item["Title"]]
-            if len(matched_items) != 1:
-                raise ProcessorError(
-                    "Could not find version %s in update metadata. "
-                    "Updates that are available: %s"
-                    % (version_str, ", ".join(["'%s'" % item["Title"]
-                                               for item in metadata])))
-            item = matched_items[0]
 
         self.env["url"] = item["Location"]
-        self.env["pkg_name"] = item["Payload"]
         self.output("Found URL %s" % self.env["url"])
         self.output("Got update: '%s'" % item["Title"])
         # now extract useful info from the rest of the metadata that could
         # be used in a pkginfo
         pkginfo = {}
-        # currently ignoring latest dict and cherry-picking en-US, may revisit
-        all_localizations = metadata[0].get("Localized")
-        pkginfo["description"] = "<html>%s</html>" % all_localizations['1033']['Short Description']
-        # pkginfo["display_name"] = item["Title"]
-        max_os = self.valueToOSVersionString(item['Max OS'])
-        min_os = self.valueToOSVersionString(item['Min OS'])
+        # Get a copy of the description in our locale_id
+        all_localizations = item.get("Localized")
+        lcid = self.env["locale_id"]
+        if lcid not in all_localizations:
+            raise ProcessorError(
+                "Locale ID %s not found in manifest metadata. Available IDs: "
+                "%s. See %s for more details." % (
+                    lcid,
+                    ", ".join(all_localizations.keys()),
+                    LOCALE_ID_INFO_URL))
+        manifest_description = all_localizations[lcid]['Short Description']
+        # Store the description in a separate output variable and in our pkginfo
+        # directly.
+        pkginfo["description"] = "<html>%s</html>" % manifest_description
+        self.env["description"] = manifest_description
+
+        max_os = self.value_to_os_version_string(item['Max OS'])
+        min_os = self.value_to_os_version_string(item['Min OS'])
         if max_os != "0.0.0":
             pkginfo["maximum_os_version"] = max_os
         if min_os != "0.0.0":
             pkginfo["minimum_os_version"] = min_os
-        installs_items = self.getInstallsItems(item)
+        installs_items = self.get_installs_items(item)
         if installs_items:
             pkginfo["installs"] = installs_items
-        if not self.env.get("munki_update_name"):
-            pkginfo['name'] = ("%s_2016_Installer" % self.env.get("product"))
-        else:
-            pkginfo['name'] = self.env["munki_update_name"]
+
+        self.env["version"] = self.get_version(item)
+        self.env["minimum_os_version"] = min_os
         self.env["additional_pkginfo"] = pkginfo
-        # re-setting so we can substitute in %20's for spaces
-        self.env["url"] = item["Location"].replace(' ', '%20')
+        self.env["url"] = item["Location"]
         self.output("Additional pkginfo: %s" % self.env["additional_pkginfo"])
 
     def main(self):
         """Get information about an update"""
-        self.getInstallerinfo()
+        self.get_installer_info()
 
 
 if __name__ == "__main__":
-    processor = MSOffice2016URLandUpdateInfoProvider()
-    processor.execute_shell()
+    PROCESSOR = MSOffice2016URLandUpdateInfoProvider()
+    PROCESSOR.execute_shell()
